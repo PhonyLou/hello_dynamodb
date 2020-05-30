eec0391cfe6592a91c98be44e71c8e6a2276a4ea
Phony Lou <louqilin555@gmail.com>
2020-05-30 20:51:47 +0800

    Add maven

---
+++ src/main/java/com/qilin/dynamodb/CreateTable.java
@@ -0,0 +1,40 @@
+package com.qilin.dynamodb;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.*;
+
+import java.util.Arrays;
+
+public class CreateTable {
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName = "Project_Qilin";
+
+        try {
+            System.out.println("Attempting to create table; please wait...");
+            Table table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("projectName", KeyType.HASH), // Partition
+                            // key
+                            new KeySchemaElement("projectType", KeyType.RANGE)), // Sort key
+                    Arrays.asList(new AttributeDefinition("projectName", ScalarAttributeType.S),
+                            new AttributeDefinition("projectType", ScalarAttributeType.S)),
+                    new ProvisionedThroughput(10L, 10L));
+            table.waitForActive();
+            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to create table: ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
