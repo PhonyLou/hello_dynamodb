87b524fbb628bf9da5c4025da4892fc3e91e9f56
Phony Lou <louqilin555@gmail.com>
2020-05-31 22:11:00 +0800

    create backup

---
+++ cli/backup_table.sh
@@ -0,0 +1,7 @@
+aws dynamodb create-backup --table-name Project_Qilin \
+ --backup-name Project_Qilin_Backup
+
+#aws dynamodb describe-backup \
+#  --backup-arn arn:aws:dynamodb:ap-southeast-1:494526681395:table/Project_Qilin/backup/01590934195074-f666fa32
+
+#aws dynamodb list-backups
